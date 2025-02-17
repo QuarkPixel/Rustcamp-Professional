cc3aebc9e2e3c5a07548cf1db60ca7746578ce08
QuarkPixel <m2462864403@gmail.com>
2025-02-17 08:45:06 +0800

    -

--- exercises/easy/algorithm3.rs
+++ exercises/easy/algorithm3.rs
@@ -1,13 +1,36 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
 
+// Quick Sort
+fn sort<T: Ord>(array: &mut [T]) {
+    if array.len() <= 1 {
+        return;
+    }
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+    let pivot_index = partition(array);
+    sort(&mut array[..pivot_index]);
+    sort(&mut array[pivot_index + 1..]);
 }
+
+fn partition<T: Ord>(array: &mut [T]) -> usize {
+    let pivot_index = array.len() / 2;
+    array.swap(pivot_index, array.len() - 1);
+    let mut i = 0;
+
+    for j in 0..array.len() - 1 {
+        if array[j] <= array[array.len() - 1] {
+            array.swap(j, i);
+            i += 1;
+        }
+    }
+
+    array.swap(i, array.len() - 1);
+    i
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,13 +41,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
